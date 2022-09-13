445717bf0e40a78e154780fb2d8a9952b21c437b
Tu Nombre <you@example.com>
2022-09-13 15:03:17 +0200

    pa

---
+++ fpga/pynq-z2/docs/udma-master/test2.py
@@ -0,0 +1,7 @@
+def txt2bin(filename= 'out.txt'):
+	with open(filename, 'r') as txt_file:
+		with open(filename[:-4]+'.bin' , 'wb') as bin_file:
+			temp = txt_file.readline().replace('\'','').replace('[','').replace(']',' ').replace('0b','').replace(',','').split()
+			bins = [int(x,2).to_bytes(4, 'big') for x in temp]
+			bin_file.write(b''.join(bins))
+txt2bin()            
--- paper/circs.py
+++ paper/circs.py
@@ -6,9 +6,9 @@ from qiskit.visualization import circuit_drawer
 from qiskit.extensions import RXGate, RYGate, RZGate
 
 circ = QuantumCircuit(2,2)
-
-J = Gate(name='J', num_qubits=2, params=[])
-J_d = Gate(name='J\dagger', num_qubits=2, params=[])
+J = Gate(name='J(\gamma)', num_qubits=2, params=[])
+J_d = Gate(name='J\dagger(\gamma)', num_qubits=2, params=[])
+U4 = Gate(name='U4(\lambda)', num_qubits=1, params=[])
 
 phia1 = Parameter('\\varphi_{A1}')
 phia2 = Parameter('\\varphi_{A2}')
@@ -22,10 +22,12 @@ circ.append(J,   [0,1])
 circ.append(RXGate(phia1), [(0)])
 circ.append(RYGate(phia2), [(0)])
 circ.append(RXGate(phia3), [(0)])
+circ.append(U4, [(0)])
 circ.append(RXGate(phib1), [(1)])
 circ.append(RYGate(phib2), [(1)])
 circ.append(RXGate(phib3), [(1)])
+circ.append(U4, [(1)])
 circ.append(J_d, [0,1])
 circ.measure([0,1], [0,1])
 
-circuit_drawer(circ, output='latex', interactive = True, scale = 2)
+circuit_drawer(circ, output='latex', interactive = True, scale = 1.5)
