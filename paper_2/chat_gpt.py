c68535f3455c525e8329f385ebdfe23f5fd3e870
Tu Nombre <you@example.com>
2022-12-27 22:53:50 -0300

    gpt

--- paper_2/chat_gpt.py
+++ paper_2/chat_gpt.py
@@ -1,228 +1,35 @@
-import gym
-import tensorflow as tf
-import numpy as np
+def find_correlated_equilibrium(payoff_matrix, max_iterations, convergence_threshold):
+  player_1_beliefs = [0.5, 0.5]
+  player_2_beliefs = [0.5, 0.5]
 
-### using neural network
+  player_1_strategy = 1
+  player_2_strategy = 1
 
-class TemperatureAgent:
-    def __init__(self):
-        # create the action environment
-        self.action_env = ActionEnv()
+  belief_difference = 1
+  iteration = 0
+  while (belief_difference > convergence_threshold) and (max_iterations > iteration):
+    player_1_strategy = player_1_beliefs.index(max(player_1_beliefs))
+    player_2_strategy = player_2_beliefs.index(max(player_2_beliefs))
 
-        # create the temperature environment
-        self.temperature_env = TemperatureEnv()
+    expected_payoff_2 = payoff_matrix[player_1_strategy][player_2_strategy][0]
+    expected_payoff_1 = payoff_matrix[player_2_strategy][player_1_strategy][1]
 
-        # create the action model
-        self.action_model = self.create_model()
+    player_1_beliefs[player_2_strategy] = player_1_beliefs[player_2_strategy] * (1 + expected_payoff_2)
+    player_2_beliefs[player_1_strategy] = player_2_beliefs[player_1_strategy] * (1 + expected_payoff_1)
 
-        # create the temperature model
-        self.temperature_model = self.create_model()
+    player_1_beliefs = [belief/sum(player_1_beliefs) for belief in player_1_beliefs]
+    player_2_beliefs = [belief/sum(player_2_beliefs) for belief in player_2_beliefs]
 
-    def create_model(self):
-        # define the neural network model
-        model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(32, input_shape=(2,), activation="relu"))
-        model.add(tf.keras.layers.Dense(32, activation="relu"))
-        model.add(tf.keras.layers.Dense(1, activation="linear"))
-        model.compile(loss="mse", optimizer="adam")
+    belief_difference = sum([abs(player_1_beliefs[i] - player_2_beliefs[i]) for i in range(2)])
+    iteration += 1
+    print(player_1_beliefs,  player_2_beliefs)
+    print(player_1_strategy, player_2_strategy)
+    print("Iteration {} => Reward of player 0 = {}. Reward of player 1 = {}.".format(iteration, expected_payoff_1, expected_payoff_2))
+  return player_1_strategy, player_2_strategy
 
-        return model
-
-    def train(self):
-        # train the action model using the action environment
-        state = self.action_env.reset()
-        for _ in range(100):
-            action_values = state[1]
-
-            # use the temperature from the temperature environment
-            temperature = self.temperature_env.temperature
-
-            # select an action using the softmax function with the temperature
-            action_probs = np.exp(action_values / temperature)
-            action_probs /= np.sum(action_probs)
-            action = np.random.choice(range(len(action_values)), p=action_probs)
-
-            next_state, reward, _, _ = self.action_env.step(action)
-            self.action_model.fit(state, reward, epochs=1, verbose=0)
-            state = next_state
-
-        # train the temperature model using the temperature environment
-        state = self.temperature_env.reset()
-        for _ in range(100):
-            action = np.random.normal()
-            next_state, reward, _, _ = self.temperature_env.step(action)
-            self.temperature_model.fit(state, reward, epochs=1, verbose=0)
-            state = next_state
-
-    def act(self):
-        # use the trained action model to select the best action
-        state = self.action_env.reset()
-        action_values = state[1]
-        temperature = self.temperature_env.temperature
-        action_probs = np.exp(action_values / temperature)
-        action_probs /= np.sum(action_probs)
-        action = np.argmax(action_probs)
-
-        # use the trained temperature model to adjust the temperature
-        action_values = self.temperature_env.reset()
-        temperature = self.temperature_model.predict(action_values)
-        self.temperature_env.temperature = np.exp(temperature)
-        return action
-
-### using q-learning
-
-class TemperatureAgent:
-    def __init__(self):
-        # create the action environment
-        self.action_env = ActionEnv()
-
-        # create the temperature environment
-        self.temperature_env = TemperatureEnv()
-
-        # create the action Q-table
-        self.action_q_table = self.create_q_table()
-
-        # create the temperature Q-table
-        self.temperature_q_table = self.create_q_table()
-
-    def create_q_table(self):
-        # define the size of the Q-table
-        state_size = 1
-        action_size = 100
-
-        # initialize the Q-table with zeros
-        q_table = np.zeros((state_size, action_size))
-
-        return q_table
-
-    def train(self):
-        # train the action model using the action environment
-        state = self.action_env.reset()
-        for _ in range(100):
-            action_values = state[1]
-
-            # use the temperature from the temperature environment
-            temperature = self.temperature_env.temperature
-
-            # select an action using the softmax function with the temperature
-            action_probs = np.exp(action_values / temperature)
-            action_probs /= np.sum(action_probs)
-            action = np.random.choice(range(len(action_values)), p=action_probs)
-
-            next_state, reward, _, _ = self.action_env.step(action)
-
-            # update the Q-value for the action
-            q_value = reward + self.discount_factor * np.max(self.action_q_table[next_state])
-            self.action_q_table[state, action] = q_value
-
-            state = next_state
-
-        # train the temperature model using the temperature environment
-        state = self.temperature_env.reset()
-        for _ in range(100):
-            action = np.random.normal()
-            next_state, reward, _, _ = self.temperature_env.step(action)
-
-            # update the Q-value for the temperature
-            q_value = reward + self.discount_factor * np.max(self.temperature_q_table[next_state])
-            self.temperature_q_table[state, action] = q_value
-
-            state = next_state
-
-    def act(self):
-        # use the trained action Q-table to select the best action
-        state = self.action_env.reset()
-        action_values = state[1]
-        temperature = self.temperature_env.temperature
-        action_probs = np.exp(action_values / temperature)
-        action_probs /= np.sum(action_probs)
-        action = np.argmax(action_probs)
-
-        # use the trained temperature Q-table to adjust the temperature
-        state = self.temperature_env.reset()
-        temperature = np.argmax(self.temperature_q_table[state])
-        self.temperature_env.temperature = temperature
-        return action
-
-### using sarsa
-
-class TemperatureAgent:
-    def __init__(self):
-        # create the action environment
-        self.action_env = ActionEnv()
-
-        # create the temperature environment
-        self.temperature_env = TemperatureEnv()
-
-        # create the action-value function
-        self.q_table = self.create_q_table()
-
-        # initialize the action policy
-        self.policy = self.create_policy()
-
-    def create_q_table(self):
-        # define the size of the action-value function
-        state_size = 1
-        action_size = 100
-        temperature_size = 100
-
-        # initialize the action-value function with zeros
-        q_table = np.zeros((state_size, action_size, temperature_size))
-
-        return q_table
-
-    def create_policy(self):
-        # define the initial action policy
-        action_space = self.action_env.action_space
-        policy = np.ones(action_space.n) / action_space.n
-
-        return policy
-
-    def train(self):
-        # train the action model using the action environment
-        state = self.action_env.reset()
-        for _ in range(100):
-            action_values = state[1]
-
-            # use the temperature from the temperature environment
-            temperature = self.temperature_env.temperature
-
-            # select an action using the softmax function with the temperature
-            action_probs = np.exp(action_values / temperature)
-            action_probs /= np.sum(action_probs)
-            action = np.random.choice(range(len(action_values)), p=action_probs)
-
-            next_state, reward, _, _ = self.action_env.step(action)
-
-            # use the temperature from the temperature environment
-            next_temperature = self.temperature_env.temperature
-
-            # select the next action using the action policy and the temperature
-            next_action_probs = np.exp(action_values / next_temperature)
-            next_action_probs /= np.sum(next_action_probs)
-            next_action = np.random.choice(range(len(action_values)), p=next_action_probs)
-
-            # update the action-value function
-            q_value = self.q_table[state, action, temperature]
-            next_q_value = self.q_table[next_state, next_action, next_temperature]
-            self.q_table[state, action, temperature] += 0.1 * (reward + 0.9 * next_q_value - q_value)
-
-            # update the action policy
-            self.policy[action] = next_q_value
-
-            state = next_state
-
-    def act(self):
-        # use the trained action policy to select the best action
-        state = self.action_env.state
-        action_values = state[1]
-
-        # use the temperature from the temperature environment
-        temperature = self.temperature_env.temperature
-
-        # select an action using the action policy and the temperature
-        action_probs = np.exp(self.policy / temperature)
-        action_probs /= np.sum(action_probs)
-        action = np.random.choice(range(len(action_values)), p=action_probs)
-
-        return action
+payoff_matrix = [[(33,33), (0,50)],
+                 [(50,0), (10,10)]]
+max_iterations = 1000
+convergence_threshold = 0.01
+equilibrium_strategies = find_correlated_equilibrium(payoff_matrix, max_iterations, convergence_threshold)
+print(equilibrium_strategies)
